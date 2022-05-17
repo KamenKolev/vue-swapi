f2c234e371a4fdbdaabfcf2f02b2dcb36d267ea7
Kamen Kolev <kkolev@pm.me>
2022-05-17 22:40:23 +0000

    formatted using prettier

--- src/data/people.service.ts
+++ src/data/people.service.ts
@@ -1,35 +1,35 @@
 type PersonDTO = {
-  id: number;
-  name: string;
-  height: number;
-  /**
-   * ISOString
-   */
-  created: string;
-  /**
-   * ISOString
-   */
-  edited: string;
-  homeworld: number;
-  mass: number | null;
-};
+	id: number
+	name: string
+	height: number
+	/**
+	 * ISOString
+	 */
+	created: string
+	/**
+	 * ISOString
+	 */
+	edited: string
+	homeworld: number
+	mass: number | null
+}
 
 export type Person = Omit<PersonDTO, "created" | "edited"> & {
-  created: Date;
-  edited: Date;
-};
+	created: Date
+	edited: Date
+}
 
 /**
  * Mutates the DTO!
  */
 const personFromDTO = (dto: PersonDTO): Person =>
-  Object.assign(dto, {
-    created: new Date(dto.created),
-    edited: new Date(dto.edited),
-  });
+	Object.assign(dto, {
+		created: new Date(dto.created),
+		edited: new Date(dto.edited),
+	})
 
 // TODO utilize env
 export const getAllPeople = (): Promise<PersonDTO[]> =>
-  fetch("http://localhost:8080/people")
-    .then((res) => res.json())
-    .then((dtos) => dtos.map(personFromDTO));
+	fetch("http://localhost:8080/people")
+		.then(res => res.json())
+		.then(dtos => dtos.map(personFromDTO))
--- src/data/planets.service.ts
+++ src/data/planets.service.ts
@@ -1,13 +1,13 @@
 type PlanetDTO = {
-  id: number;
-  name: string;
-  diameter: number;
-  climate: string;
-  population: number;
-};
+	id: number
+	name: string
+	diameter: number
+	climate: string
+	population: number
+}
 
-export type Planet = PlanetDTO;
+export type Planet = PlanetDTO
 
 // TODO utilize env
 export const getAllPlanets = (): Promise<Planet[]> =>
-  fetch("http://localhost:8080/planets").then((res) => res.json());
+	fetch("http://localhost:8080/planets").then(res => res.json())
